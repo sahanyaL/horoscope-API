6b8e370309b838a65bb7f5ad9cacbab7ddbde92c
sahanyaL <132870882+sahanyaL@users.noreply.github.com>
2026-02-06 08:04:31 +0530

    Flask server is officially running with Swagger UI

---
+++ core/__init__.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from decouple import config
+from flask_restx import Api
+
+app = Flask(__name__)
+app.config.from_object(config("APP_SETTINGS"))
+api = Api(
+    app,
+    version='1.0',
+    title='Horoscope API',
+    description='Get horoscope data easily using the below APIs',
+    doc='/',
+    prefix='/api/v1'
+)
+
+from core import routes
